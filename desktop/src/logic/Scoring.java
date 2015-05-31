2074867b09d07432514655e7a03f6859ebcdf760
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-05-31 21:28:56 +0100

    Terminado: Class de Pontuação
    Vou fazer a classe de Jogo e de Jogador a seguir.

--- desktop/src/logic/Scoring.java
+++ desktop/src/logic/Scoring.java
@@ -1,7 +1,7 @@
 package logic;
 
 public class Scoring {
-	
+
 	private class Frame
 	{
 		int frame_number;
@@ -9,11 +9,11 @@ public class Scoring {
 		int secondChance;
 		int thirdChance;	
 	}
-	
-	
+
+
 	private Frame[] scoreChart = new Frame[11];
 	private int nextPlay = 0;
-	
+
 	public Scoring(){
 		for (int i = 1; i <= 10 ; i++ )
 		{
@@ -23,11 +23,11 @@ public class Scoring {
 			scoreChart[i].secondChance = 0;
 			scoreChart[i].thirdChance = 0;
 		}
-		
+
 		nextPlay = 1;
 	}
-	
-	
+
+
 	public int getNextPlay()
 	{
 		return nextPlay;
@@ -48,7 +48,7 @@ public class Scoring {
 			FramePlay = (PlayNumber+1) / 2;
 			ChanceNumber = 2 - (PlayNumber % 2);
 		}
-		
+
 		if (ChanceNumber == 3)
 			return scoreChart[FramePlay].thirdChance;
 		else if(ChanceNumber == 2)
@@ -56,6 +56,137 @@ public class Scoring {
 		else
 			return scoreChart[FramePlay].firstChance;
 	}
-	
-	
+
+	public boolean makePlay(int pinsDown)
+	{
+		if (nextPlay < 1 || nextPlay > 21 || pinsDown < 0 || pinsDown > 10)
+			return false;
+
+		if (nextPlay == 20)
+		{
+			scoreChart[10].secondChance = pinsDown;
+			if (scoreChart[10].firstChance == 10 || (scoreChart[10].secondChance+scoreChart[10].secondChance) == 10)
+				nextPlay++;
+			else
+				nextPlay = -1;
+			return true;
+		}
+
+		if (nextPlay == 21)
+		{
+			scoreChart[10].thirdChance = pinsDown;
+			nextPlay = -1;
+			return true;
+		}
+
+		int FramePlay; int ChanceNumber;
+		FramePlay = (nextPlay+1) / 2;
+		ChanceNumber = 2 - (nextPlay % 2);
+
+
+		if(ChanceNumber == 2)
+			scoreChart[FramePlay].secondChance = pinsDown;
+		else
+		{
+			scoreChart[FramePlay].firstChance = pinsDown;
+			if (pinsDown == 10 && nextPlay != 19) //Strike, skip the second play
+				nextPlay++;			
+		}	
+		nextPlay++;	
+
+		return true;
+	}
+
+	public void restartScoring()
+	{
+		for (int i = 1; i <= 10; i++)
+		{
+			scoreChart[i].firstChance = 0;
+			scoreChart[i].secondChance = 0;
+			scoreChart[i].thirdChance = 0;
+			nextPlay = 1;
+		}
+	}
+
+	public int latestScoredFrame()
+	{
+		if (nextPlay == -1)
+			return 10;
+		if (nextPlay == 21)
+			return 9;
+
+		int FramePlay;
+		FramePlay = (nextPlay+1) / 2;
+
+		return FramePlay-1;
+
+	}
+
+	public int getScoreFrame(int frameNumber)
+	{
+		if (frameNumber > 10 || frameNumber < 0 || frameNumber > latestScoredFrame())
+			return -1;
+
+		int score = 0;
+
+		if (frameNumber < 9)
+		{
+			score += scoreChart[frameNumber].firstChance;
+			if (score == 10)// Strike
+			{ 
+				score += scoreChart[frameNumber+1].firstChance;
+				if(score == 20) //Nextframe was a strike
+				{
+					score += scoreChart[frameNumber+2].firstChance;
+				} else
+				{
+					score += scoreChart[frameNumber+1].secondChance;
+				}
+			} else //Not a Strike
+			{ 
+				score += scoreChart[frameNumber].secondChance;
+				if (score == 10) //First Spare
+				{
+					score += scoreChart[frameNumber+1].firstChance;
+				}
+			}
+		}
+
+		if (frameNumber == 9)
+		{
+			score += scoreChart[frameNumber].firstChance;
+			if (score == 10)// Strike
+			{ 
+				score += scoreChart[frameNumber+1].firstChance;
+				score += scoreChart[frameNumber+1].secondChance;
+			} else //Not a Strike
+			{ 
+				score += scoreChart[frameNumber].secondChance;
+				if (score == 10) //First Spare
+				{
+					score += scoreChart[frameNumber+1].firstChance;
+				}
+			}
+		}
+		
+		if (frameNumber == 10)
+		{
+			score += scoreChart[frameNumber].firstChance;
+			if (score == 10)// Strike
+			{ 
+				score += scoreChart[frameNumber].secondChance;
+				score += scoreChart[frameNumber].thirdChance;
+			} else 
+			{ 
+				score += scoreChart[frameNumber].secondChance;
+				if (score == 10) // Spare
+					score += scoreChart[frameNumber].thirdChance;
+			}
+		}
+
+
+		return score;
+	}
+
+
 }
--- desktop/src/test/TestLogic.java
+++ desktop/src/test/TestLogic.java
@@ -8,13 +8,57 @@ import logic.*;
 public class TestLogic {
 
 	@Test
-	public void test_ScoreTableCreation() {
+	public void test_ScoreTable() {
 		Scoring test = new Scoring();
 		for (int i = 1; i <= 21; i++ )
 			assertEquals("Sucesso!",  test.getPinsFelled(i), 0);
-		
 		assertEquals("Sucesso!",  test.getPinsFelled(0), -1);
 		assertEquals("Sucesso!",  test.getPinsFelled(22), -1);
+		assertEquals(test.makePlay(-1), false);
+		assertEquals(test.makePlay(11), false);
+		for (int i = 1; i <= 20; i++)
+		{
+			assertEquals(test.makePlay(1), true);
+		}
+		assertEquals(test.makePlay(0), false);
+		
+		test.restartScoring();
+		for (int i = 1; i <= 10; i++)
+		{
+			assertEquals(test.latestScoredFrame(), i-1);
+			assertEquals(test.makePlay(10), true);
+		}
+		assertEquals(test.latestScoredFrame(), 9);
+		assertEquals(test.makePlay(10), true);
+		assertEquals(test.latestScoredFrame(), 9);
+		assertEquals(test.makePlay(10), true);
+		assertEquals(test.latestScoredFrame(), 10);
+		assertEquals(test.makePlay(10), false);
+		int expectedscore = 0;
+		for (int i = 1; i <= 10; i++)
+		{
+			expectedscore += test.getScoreFrame(i); 
+			assertEquals(i*30, expectedscore);
+		}
+		
+		
+		test.restartScoring();
+		assertEquals(test.makePlay(1), true); //1
+		assertEquals(test.makePlay(1), true);
+		assertEquals(2, test.getScoreFrame(1));
+		assertEquals(test.makePlay(1), true); //2
+		assertEquals(test.makePlay(9), true);
+		assertEquals(10, test.getScoreFrame(2));
+		assertEquals(test.makePlay(1), true); //3
+		assertEquals(test.makePlay(1), true);
+		assertEquals(11, test.getScoreFrame(2));
+		assertEquals(2, test.getScoreFrame(3));
+		assertEquals(test.makePlay(10), true); //4
+		assertEquals(test.makePlay(10), true); //5
+		assertEquals(test.makePlay(1), true); //6
+		assertEquals(test.makePlay(1), true);
+		assertEquals(21, test.getScoreFrame(4));
+		assertEquals(12, test.getScoreFrame(5));
 		
 	}
 
