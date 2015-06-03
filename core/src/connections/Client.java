9c94d4c35d37f2bb6c2cbc861575724c28d6cdcf
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-06-03 13:53:36 +0100

    Connections
    e testes

--- core/src/connections/Client.java
+++ core/src/connections/Client.java
@@ -1,5 +1,150 @@
 package connections;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class Client {
+	
+	public Boolean isConnected = false;
+	public Boolean readReady = false;
+	public int playerNumber;
+	
+	private Socket clientSocket;
+
+	private BufferedReader  inputFromServer;
+	private PrintStream outputToServer;
+
+	private Thread connect_Server = new Thread();
+	private Thread receive_Server = new Thread();
+
+	private String messageServer;
+	private float valueServer;
+	private DataPacket Data = new DataPacket();
+
+	private String EXTERNAL_IP;
+	private final int IP_PORT = 34567; 
+
+	
+	public Client(String s)
+	{
+		EXTERNAL_IP = s;
+	}
+	
+	public Boolean connectServer()
+	{
+		if (connect_Server.isAlive() == false &&  !isConnected)
+		{
+			connect_Server = new Thread(new connectServerThread());
+			connect_Server.start();
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean startListeningPlayers()
+	{
+		if (receive_Server.isAlive() == false && isConnected)
+		{
+			receive_Server = new Thread(new receiveServerThread());
+			receive_Server.start();
+			return true;
+		}
+		return false;
+	}
+	
+	public DataPacket getLatestData(){
+		if (readReady)
+		{
+			readReady = false;
+			return Data;
+		}
+		return null;
+	}
+	
+	public Boolean disconnectServer() throws IOException{
+		if (connect_Server.isAlive() == false && isConnected == true)
+		{
+			isConnected = false;
+			readReady = false;
+			clientSocket.close();
+			return true;
+		}
+		return false;
+		
+	}
+
+	public void sendMessageServer(String event, float value)
+	{
+		if (isConnected == true)
+		{
+			outputToServer.println(event);
+			outputToServer.println(Float.toString(value));	
+		}
+	}
+	
+	private class connectServerThread implements Runnable
+	{
+		public void run() {
+			try {
+				clientSocket =  new Socket(InetAddress.getByName(EXTERNAL_IP), IP_PORT);
+				inputFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				outputToServer = new PrintStream(clientSocket.getOutputStream());
+
+
+				//Server should send Player Number Info
+				while(!inputFromServer.ready())
+					;
+				messageServer = inputFromServer.readLine();
+				if (messageServer.matches("Player"))
+				{
+					while(!inputFromServer.ready())
+						;
+					playerNumber = Integer.parseInt(inputFromServer.readLine());
+					
+					isConnected = true;
+				}
+				else
+				{
+					playerNumber = -1;
+				}
+					
+				
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private class receiveServerThread implements Runnable
+	{
+		public void run() {
+
+			while(isConnected)
+			{
+				try {
+					if (inputFromServer.ready() && !readReady)
+					{
+						
+						messageServer = inputFromServer.readLine();
+						while(!inputFromServer.ready())
+							;
+						valueServer = Float.parseFloat(inputFromServer.readLine());
+
+						Data.setData(messageServer, valueServer);
+
+						readReady = true;
+
+					}
+				}	
+				catch (NumberFormatException e) {e.printStackTrace();} 
+				catch (IOException e) {e.printStackTrace();}
+			}
+
+		}
+	}
+	
 }
--- core/src/connections/Server.java
+++ core/src/connections/Server.java
@@ -4,17 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+	
+	public Boolean player1_isConnected = false;
+	public Boolean player2_isConnected = false;
+
+	public Boolean readPlayer1 = false;
+	public Boolean readPlayer2 = false;
+	
 
 	private ServerSocket serverSocket;
 	private Socket clientSocket[] = new Socket[2];
-
-	private Boolean player1_isConnected = false;
-	private Boolean player2_isConnected = false;
 
 	private BufferedReader inputFromPlayer1;
 	private BufferedReader inputFromPlayer2;
@@ -25,14 +28,14 @@ public class Server {
 	private Thread connect_Player2 = new Thread();
 	private Thread receive_Player1 = new Thread();
 	private Thread receive_Player2 = new Thread();
-	
+
 	private String messagePlayer1;
 	private String messagePlayer2;
 	private float valuePlayer1;
 	private float valuePlayer2;
-	private DataPacket Data1;
-	private DataPacket Data2;
-	
+	private DataPacket Data1 = new DataPacket();
+	private DataPacket Data2 = new DataPacket();
+
 	private final int IP_PORT = 34567; 
 
 	public Server() throws IOException
@@ -42,13 +45,13 @@ public class Server {
 
 	public Boolean connectPlayers(int PlayerNumber) throws IOException
 	{
-		if (PlayerNumber == 1 && connect_Player1.isAlive() == false)
+		if (PlayerNumber == 1 && connect_Player1.isAlive() == false && !player1_isConnected)
 		{
 			connect_Player1 = new Thread(new connectPlayer1());
 			connect_Player1.start();
 			return true;
 		}
-		if (PlayerNumber == 2 && connect_Player2.isAlive() == false)
+		if (PlayerNumber == 2 && connect_Player2.isAlive() == false && !player2_isConnected)
 		{
 			connect_Player2 = new Thread(new connectPlayer2());
 			connect_Player2.start();
@@ -76,31 +79,39 @@ public class Server {
 	}
 
 	public DataPacket getLatestDataPlayers(int PlayerNumber){
-		if(PlayerNumber == 1 && receive_Player1.isAlive() == true && player1_isConnected == true)
+		if(PlayerNumber == 1 && readPlayer1)
+		{
+			readPlayer1 = false;
 			return Data1;
-		if(PlayerNumber == 2 && receive_Player2.isAlive() == true && player2_isConnected == true)
+		}
+		if(PlayerNumber == 2 && readPlayer2)
+		{
+			readPlayer2 = false;
 			return Data2;
+		}
 		
 		return null;
 	}
-	
+
 	public Boolean disconnectPlayer(int PlayerNumber) throws IOException{
 		if (PlayerNumber == 1 && connect_Player1.isAlive() == false && player1_isConnected == true)
 		{
 			player1_isConnected = false;
+			readPlayer1 = false;
 			clientSocket[0].close();
 			return true;
 		}
 		if (PlayerNumber == 2 && connect_Player2.isAlive() == false && player2_isConnected == true)
 		{
 			player2_isConnected = false;
+			readPlayer2 = false;
 			clientSocket[1].close();
 			return true;
 		}
 		return false;
-		
+
 	}
-	
+
 	public void sendMessagePlayer(int PlayerNumber, String event, float value)
 	{
 		if (PlayerNumber == 1 && player1_isConnected == true)
@@ -114,7 +125,7 @@ public class Server {
 			outputToPlayer2.println(Float.toString(value));	
 		}
 	}
-	
+
 
 	private class connectPlayer1 implements Runnable
 	{
@@ -132,8 +143,6 @@ public class Server {
 
 				player1_isConnected = true;
 
-
-
 			} catch (IOException e) {e.printStackTrace();}
 		}
 	}
@@ -149,7 +158,7 @@ public class Server {
 
 				//Send Player Number to Socket
 				outputToPlayer2.println("Player");
-				outputToPlayer2.println(Integer.toString(1));
+				outputToPlayer2.println(Integer.toString(2));
 
 				player2_isConnected = true;
 
@@ -161,23 +170,23 @@ public class Server {
 	private class receivePlayer1 implements Runnable
 	{
 		public void run() {
-
 			while(player1_isConnected)
 			{
 				try {
-					if (inputFromPlayer1.ready())
+					if (inputFromPlayer1.ready() && !readPlayer1)
 					{
-						//Freedom Version
 						messagePlayer1 = inputFromPlayer1.readLine();
-						while(!inputFromPlayer1.ready() && player1_isConnected)
+						while(!inputFromPlayer1.ready())
 							;
 						valuePlayer1 = Float.parseFloat(inputFromPlayer1.readLine());
 						Data1.setData(messagePlayer1, valuePlayer1);
-						
+
+						readPlayer1 = true;
+
 						//Controlled
 						/*
 							 String input = inputFromPlayer2.readLine();
-	
+
 							if (input.matches("IMPLEMENTSOMETHING"))
 							{
 								while(!inputFromPlayer2.ready() && player2_isConnected)
@@ -187,9 +196,9 @@ public class Server {
 						 */
 					}
 				}	catch (NumberFormatException e) {e.printStackTrace();} 
-					catch (IOException e) {e.printStackTrace();}
+				catch (IOException e) {e.printStackTrace();}
 			}
-			
+
 		}
 	}
 	private class receivePlayer2 implements Runnable
@@ -199,21 +208,23 @@ public class Server {
 			while(player2_isConnected)
 			{
 				try {
-					if (inputFromPlayer2.ready())
+					if (inputFromPlayer2.ready() && !readPlayer2)
 					{
 						//Freedom Version
 						messagePlayer2 = inputFromPlayer2.readLine();
-						while(!inputFromPlayer2.ready() && player2_isConnected)
+						while(!inputFromPlayer2.ready())
 							;
 						valuePlayer2 = Float.parseFloat(inputFromPlayer2.readLine());
-						
+
 						Data2.setData(messagePlayer2, valuePlayer2);
-						
+
+						readPlayer2 = true;
+
 					}
 				}	catch (NumberFormatException e) {e.printStackTrace();} 
-					catch (IOException e) {e.printStackTrace();}
+				catch (IOException e) {e.printStackTrace();}
 			}
-			
+
 		}
 	}
 
